74792d547660b58bcd79d29c9bd5e61475f8f6dd
amit priyadarshi <amit.sinha.priyadarshi@gmail.com>
2025-10-17 23:36:50 +0100

    logger and exception

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='job_scanner',
+    version='0.1',
+    description='A job scanning tool',
+    author='Amit Priyadarshi', 
+    packages=find_packages(),
+)
+
